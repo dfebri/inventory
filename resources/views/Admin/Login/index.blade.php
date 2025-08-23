9d9eb5d7ddf2bebc8995ec8a103b98157fb19bb4
febri <dwifebrimurcahyo@gmail.com>
2025-08-23 23:39:40 +0700

    update login

--- resources/views/Admin/Login/index.blade.php
+++ resources/views/Admin/Login/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-<div class="container-login50 d-flex" style="background: linear-gradient(to right, #e9eaec, #186ef7);">
-    <div class="wrap-login100 p-2 w-20" style="background: rgb(255, 255, 255); border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); margin: auto;">
-        <div class="d-flex justify-content-center align-items-center mb-2">
+<div class="container-login100">
+    <div class="wrap-login100 p-6">
+        <div class="d-flex justify-content-center align-items-center">
             @if($web->web_logo == '' || $web->web_logo == 'E-ATK.png')
             <img src="{{url('/assets/default/web/E-ATK.png')}}" height="75px" class="" alt="logo">
             @else
@@ -12,47 +12,44 @@
             @endif
         </div>
         <div class="text-center">
-            <h4 class="fw-bold text-black text-uppercase text-truncate">{{$web->web_nama}} <span class="text-gray">| LOGIN</span></h4>
+            <h4 class="fw-bold mt-4 text-black text-uppercase text-truncate">{{$web->web_nama}} <span class="text-gray">| LOGIN</span></h4>
         </div>
-        <form class="login100-form validate-form mt-3" method="POST" name="myForm" action="{{ url('admin/proseslogin') }}" enctype="multipart/form-data" onsubmit="return validateForm()">
+        <form class="login100-form validate-form" method="POST" name="myForm" action="{{ url('admin/proseslogin') }}" enctype="multipart/form-data" onsubmit="return validateForm()">
             @csrf
             <div class="panel panel-primary">
-                <div class="panel-body tabs-menu-body p-0 pt-3">
+                <div class="panel-body tabs-menu-body p-0 pt-5">
                     <div class="tab-content">
                         <div class="tab-pane active" id="tab5">
-                            <div class="wrap-input100 validate-input input-group mb-3" data-bs-validate="Valid username is required">
+                            <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid username is required">
                                 <a tabindex="-1" href="javascript:void(0)" class="input-group-text bg-white text-muted">
                                     <i class="zmdi zmdi-account text-muted ms-1" aria-hidden="true"></i>
                                 </a>
                                 <input name="user" value="{{Session::get('userInput')}}" class="input100 border-start-0 form-control ms-0" type="text" placeholder="Username" autocomplete="on">
                             </div>
-                            <div class="wrap-input100 validate-input input-group mb-4" id="Password-toggle">
+                            <div class="wrap-input100 validate-input input-group" id="Password-toggle">
                                 <a tabindex="-1" href="javascript:void(0)" class="input-group-text bg-white text-muted">
                                     <i class="zmdi zmdi-eye text-muted" aria-hidden="true"></i>
                                 </a>
                                 <input name="pwd" class="input100 border-start-0 form-control ms-0" type="password" placeholder="Password" autocomplete="off">
                             </div>
+                            <!-- <div class="text-end pt-4">
+                                <p class="mb-0"><a href="forgot-password.html" class="text-primary ms-1">Forgot Password?</a></p>
+                            </div> -->
                             <div class="container-login100-form-btn">
                                 <button type="button" class="login100-form-btn btn btn-primary d-none" id="btnLoader" type="button" disabled="">
                                     <span class="spinner-border spinner-border-sm me-1" role="status" aria-hidden="true"></span>
                                     Loading...
                                 </button>
-                                <button type="submit" class="login100-form-btn btn btn-blue" id="btnLogin">Login</button>
+                                <button type="submit" class="login100-form-btn btn btn-primary" id="btnLogin">Login</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-                <!-- <marquee>Kamu jangan lupa makan yah! &#128522</marquee> -->
         </form>
     </div>
-    <div class="w-50 d-flex justify-content-center">
-        <img src="{{url('/assets/images/brand/ATK10.jpg')}}" class="img-fluid vh-100" alt="side image" style="object-fit: cover; width: 100%; border-radius: 3px; margin: auto;">
-    </div>
 </div>
-
 @endsection
-
 @section('scripts')
 
 <script>
